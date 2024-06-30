e6ac18e60e46cc4dc4bb404659e381a4da3954b2
sonukr0 <sonu.kumar@eruna.co.uk>
2024-06-30 20:26:16 +0530

    Update HelloWorldController.java

--- src/main/java/com/test/sone/HelloWorldController.java
+++ src/main/java/com/test/sone/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @GetMapping("/helloworld")
     public String getHelloWString(){
-        return "Welcome to eruna.!!";
+        return "Welcome to eruna..!!";
     }
 
 }
