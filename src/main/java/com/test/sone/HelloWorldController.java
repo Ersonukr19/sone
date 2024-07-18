aa022126448694927fd7d7dfbb50a683de301a3a
Sonu Kumar <ersonukumar19@gmail.com>
2024-07-18 19:58:27 +0530

    Update HelloWorldController.java

--- src/main/java/com/test/sone/HelloWorldController.java
+++ src/main/java/com/test/sone/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
     @GetMapping("/helloworld")
     public String getHelloWString(){
-        return "Welcome-4";
+        return "Welcome Spring_Boot";
     }
 
 }
